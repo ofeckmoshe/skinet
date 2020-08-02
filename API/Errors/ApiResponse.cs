74dae58bcb17280df769c2a985f6f9eb3af874f4
ofeck <ofeckm3@gmail.com>
2020-08-02 17:26:06 +0300

    section 4

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "Bad Request",
+                401 => "Unauthorized",
+                404 => "Resource Not Found",
+                500 => "Internal Server Error",
+                _ => null
+            };
+        }
+    }
+}
---
+++ API/Middleware/ExceptionMiddelware.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API.Errors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddelware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddelware> _logger;
+        private readonly IHostingEnvironment _env;
+        public ExceptionMiddelware(RequestDelegate next, ILogger<ExceptionMiddelware> logger,
+        IHostingEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException((int)HttpStatusCode.InternalServerError, ex.Message,
+                        ex.StackTrace.ToString())
+                    : new ApiException((int)HttpStatusCode.InternalServerError);
+
+                var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+}
+}
