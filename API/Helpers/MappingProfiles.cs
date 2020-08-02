05e771f76cebf6dd6a383bacfc828ad306f55f04
ofeck <ofeckm3@gmail.com>
2020-08-02 13:13:36 +0300

    section 3

---
+++ API/Helpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductToReturnDto>()
+                .ForMember(destination => destination.ProductBrand, options => options.MapFrom(source => source.ProductBrand.Name))
+                .ForMember(destination => destination.ProductType, options => options.MapFrom(source => source.ProductType.Name))
+                .ForMember(destination => destination.PictureUrl, options => options.MapFrom<ProductUrlResolver>());
+        }
+    }
+}
